729a0e1302eda48122c3deeceb9c9e39db3b53ea
anaverage-enri <anaverage.enri@gmail.com>
2026-05-31 22:27:42 +0700

    refactor(nvim): group line-number options into an Options section

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,10 @@
-vim.opt.number = true
-vim.opt.relativenumber = true
-
 vim.cmd.colorscheme("catppuccin")
 
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+-- ==========
+-- Options
+-- ==========
+vim.opt.number = true -- line number
+vim.opt.relativenumber = true -- relative line number
