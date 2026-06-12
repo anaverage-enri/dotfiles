a0fdffabdcec2376f130073821c91865e4eff366
anaverage-enri <anaverage.enri@gmail.com>
2026-06-12 22:21:43 +0700

    refactor(nvim): wire opts config file to luna config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,21 +4,4 @@ vim.cmd.colorscheme("catppuccin")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
--- ==========
--- Options
--- ==========
-vim.opt.number = true -- line number
-vim.opt.relativenumber = true -- relative line number
-vim.opt.cursorline = true -- highlight the current line
-vim.opt.wrap = false -- do not wrap lines by default
-vim.opt.scrolloff = 10 -- keep 10 lines above/below cursor
-vim.opt.sidescrolloff = 10 -- keep 10 lines left/right of cursor
-
-vim.opt.tabstop = 2 -- tab width
-vim.opt.shiftwidth = 2 -- indent width
-vim.opt.softtabstop = 2 -- soft tab stop not tabs on tab/backspace
-vim.opt.expandtab = true -- use spaces instead of tabs
-vim.opt.smartindent = true -- smart auto-indent
-vim.opt.autoindent = true -- copy indent from current line
-
-vim.opt.showmatch = true -- highlights matching brackets
+require('options')
